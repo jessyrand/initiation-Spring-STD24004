8756baa276b4c36e2ee51c981f1116051d3580f2
jessyrand <henintsoarandriam@gmail.com>
2026-03-12 09:40:40 +0300

    feat: add GreetingController class

---
+++ src/main/java/school/hei/initiationspring/restservice/RestServiceApplication.java
@@ -0,0 +1,10 @@
+package school.hei.initiationspring.restservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
