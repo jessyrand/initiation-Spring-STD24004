37e9f92209a68a76326e8c4fd480426c39c16c44
jessyrand <henintsoarandriam@gmail.com>
2026-03-12 09:17:01 +0300

    initial commit

---
+++ src/main/java/school/hei/initiationspring/InitiationSpringApplication.java
@@ -0,0 +1,13 @@
+package school.hei.initiationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitiationSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/initiationspring/InitiationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.initiationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitiationSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
