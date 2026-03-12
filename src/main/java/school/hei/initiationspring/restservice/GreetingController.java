aedc53487c0fea1a14021bb9933221c93f0d74ac
jessyrand <henintsoarandriam@gmail.com>
2026-03-12 09:55:17 +0300

    fix: add missing annotations and GreetingController test

--- src/main/java/school/hei/initiationspring/restservice/GreetingController.java
+++ src/main/java/school/hei/initiationspring/restservice/GreetingController.java
@@ -2,9 +2,11 @@ package school.hei.initiationspring.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
--- src/main/java/school/hei/initiationspring/restservice/RestServiceApplication.java
+++ src/main/java/school/hei/initiationspring/restservice/RestServiceApplication.java
@@ -1,7 +1,9 @@
 package school.hei.initiationspring.restservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RestServiceApplication {
 
     public static void main(String[] args) {
